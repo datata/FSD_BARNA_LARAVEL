ede210a09582df6468d07a33d4d84cd318b53c4d
Dani <d.tarazoma@geekshubs.com>
2023-01-26 09:28:58 +0100

    feat: task model & create task into db

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,20 +19,42 @@ Route::get('/welcome', function () {
     return 'Bienvenidos a mi aplicacion de tareas';
 });
 
-Route::post('/tasks', function () {
-    return 'Crear Tarea';
+Route::post('/tasks', function (Request $request) {
+    try {
+        //Recuperar informacion del body
+        $title = $request->input('title');
+        $description = $request->input('description');
+
+        //Guardar en bd la nueva tarea
+        $task = new Task();
+        $task->title = $title;
+        $task->description = $description;
+        $task->save();
+
+        // response
+        return response([
+            "success" => true,
+            "message" => "Task created successfuly",
+            "data" => $task
+        ], 200);
+    } catch (\Throwable $th) {
+        return response([
+            "success" => false,
+            "message" => "Error creating task: ".$th->getMessage()
+        ], 500);
+    }
 });
 
 Route::put('/tasks/{id}', function ($id) {
-    return 'Actualizar Tarea '.$id;
+    return 'Actualizar Tarea ' . $id;
 });
 
 Route::delete('/tasks/{id}', function ($id) {
-    return 'Eliminar Tarea: '.$id;
+    return 'Eliminar Tarea: ' . $id;
 });
 
 Route::get('/tasks/{id}', function ($id) {
-    return 'Recuperar Tarea por id: '.$id;
+    return 'Recuperar Tarea por id: ' . $id;
 });
 
 Route::get('/tasks', function () {
